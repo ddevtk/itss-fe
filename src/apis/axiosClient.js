e2636144ab8fd3b8e75fab403b11d6ab4ef59209
ddevtk <buiducduong2342000@gmail.com>
2022-12-22 00:47:13 +0700

    revert

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import queryString from 'query-string';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000/apis/',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_API_LOCAL_BASE_URL
+      : process.env.REACT_APP_API_BASE_URL,
   headers: {
     'content-type': 'application/json',
   },
